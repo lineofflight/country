9078b19cf2ea1430501c5b1373472b3412df1885
amirsafiallah <amirsafiallah@gmail.com>
2025-03-30 16:49:21 +0330

    Single core

--- server.js
+++ server.js
@@ -1,25 +1,11 @@
-const cluster = require("cluster")
-const os = require("os")
 const { initializeDatabase, scheduleDatabaseUpdates } = require("./dbInit")
 
-if (cluster.isMaster) {
-  // Initialize and schedule database updates
-  initializeDatabase()
-  scheduleDatabaseUpdates()
+// Initialize and schedule database updates
+initializeDatabase()
+scheduleDatabaseUpdates()
+const app = require("./app")
 
-  // Fork workers
-  for (let i = 0; i < os.cpus().length; i++) {
-    cluster.fork()
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`)
-  })
-} else {
-  const app = require("./app")
-
-  const port = process.env.PORT || 3000
-  app.listen(port, () => {
-    console.log(`Worker ${cluster.worker.id} listening on port ${port}`)
-  })
-}
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
